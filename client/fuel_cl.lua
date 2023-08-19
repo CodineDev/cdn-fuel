f57519a7f82e406db4795a4eb5a9c21ef6543075
dnelyk <95599217+dnelyk@users.noreply.github.com>
2023-08-18 22:41:02 -0700

    feat(2.1.9 | Fix OX_Target AddTargetModel Error)

--- client/fuel_cl.lua
+++ client/fuel_cl.lua
@@ -2622,73 +2622,77 @@ CreateThread(function()
 
 		exports.ox_target:addGlobalVehicle(options)
 
-		exports['qb-target']:AddTargetModel(props, {
-			options = {
-				{
-					num = 1,
-					type = "client",
-					event = "cdn-fuel:client:grabnozzle",
-					icon = "fas fa-gas-pump",
-					label = Lang:t("grab_nozzle"),
-					canInteract = function()
-						if PlayerInSpecialFuelZone then return false end
-						if not IsPedInAnyVehicle(PlayerPedId()) and not holdingnozzle and not HoldingSpecialNozzle and inGasStation == true and not PlayerInSpecialFuelZone then
-							return true
-						end
-					end,
-				},
-				{
-					num = 2,
-					type = "client",
-					event = "cdn-fuel:client:purchasejerrycan",
-					icon = "fas fa-fire-flame-simple",
-					label = Lang:t("buy_jerrycan"),
-					canInteract = function()
-						if not IsPedInAnyVehicle(PlayerPedId()) and not holdingnozzle and not HoldingSpecialNozzle and inGasStation == true then
-							return true
-						end
-					end,
-				},
-				{
-					num = 3,
-					type = "client",
-					event = "cdn-fuel:client:returnnozzle",
-					icon = "fas fa-hand",
-					label = Lang:t("return_nozzle"),
-					canInteract = function()
-						if holdingnozzle and not refueling then
-							return true
-						end
-					end,
-				},
-				{
-					num = 4,
-					type = "client",
-					event = "cdn-fuel:client:grabnozzle:special",
-					icon = "fas fa-gas-pump",
-					label = Lang:t("grab_special_nozzle"),
-					canInteract = function()
-						if Config.FuelDebug then print("Is Player In Special Fuel Zone?: "..tostring(PlayerInSpecialFuelZone)) end
-						if not HoldingSpecialNozzle and not IsPedInAnyVehicle(PlayerPedId()) and PlayerInSpecialFuelZone then
-							return true
-						end
-					end,
-				},
-				{
-					num = 5,
-					type = "client",
-					event = "cdn-fuel:client:returnnozzle:special",
-					icon = "fas fa-hand",
-					label = Lang:t("return_special_nozzle"),
-					canInteract = function()
-						if HoldingSpecialNozzle and not IsPedInAnyVehicle(PlayerPedId()) then
-							return true
-						end
+		local modelOptions = {
+			[1] = {
+				name = "cdn-fuel:modelOptions:option_1",
+				num = 1,
+				type = "client",
+				event = "cdn-fuel:client:grabnozzle",
+				icon = "fas fa-gas-pump",
+				label = Lang:t("grab_nozzle"),
+				canInteract = function()
+					if PlayerInSpecialFuelZone then return false end
+					if not IsPedInAnyVehicle(PlayerPedId()) and not holdingnozzle and not HoldingSpecialNozzle and inGasStation == true and not PlayerInSpecialFuelZone then
+						return true
 					end
-				},
+				end,
 			},
-			distance = 2.0
-		})
+			[2] = {
+				name = "cdn-fuel:modelOptions:option_2",
+				num = 2,
+				type = "client",
+				event = "cdn-fuel:client:purchasejerrycan",
+				icon = "fas fa-fire-flame-simple",
+				label = Lang:t("buy_jerrycan"),
+				canInteract = function()
+					if not IsPedInAnyVehicle(PlayerPedId()) and not holdingnozzle and not HoldingSpecialNozzle and inGasStation == true then
+						return true
+					end
+				end,
+			},
+			[3] = {
+				name = "cdn-fuel:modelOptions:option_3",
+				num = 3,
+				type = "client",
+				event = "cdn-fuel:client:returnnozzle",
+				icon = "fas fa-hand",
+				label = Lang:t("return_nozzle"),
+				canInteract = function()
+					if holdingnozzle and not refueling then
+						return true
+					end
+				end,
+			},
+			[4] = {
+				name = "cdn-fuel:modelOptions:option_4",
+				num = 4,
+				type = "client",
+				event = "cdn-fuel:client:grabnozzle:special",
+				icon = "fas fa-gas-pump",
+				label = Lang:t("grab_special_nozzle"),
+				canInteract = function()
+					if Config.FuelDebug then print("Is Player In Special Fuel Zone?: "..tostring(PlayerInSpecialFuelZone)) end
+					if not HoldingSpecialNozzle and not IsPedInAnyVehicle(PlayerPedId()) and PlayerInSpecialFuelZone then
+						return true
+					end
+				end,
+			},
+			[5] = {
+				name = "cdn-fuel:modelOptions:option_5",
+				num = 5,
+				type = "client",
+				event = "cdn-fuel:client:returnnozzle:special",
+				icon = "fas fa-hand",
+				label = Lang:t("return_special_nozzle"),
+				canInteract = function()
+					if HoldingSpecialNozzle and not IsPedInAnyVehicle(PlayerPedId()) then
+						return true
+					end
+				end
+			},
+		}
+
+		exports.ox_target:addModel(props, modelOptions)
 	else
 		exports[Config.TargetResource]:AddTargetBone(bones, {
 			options = {
